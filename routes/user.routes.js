4193c12d1d9b4b175c119ab8072195fc999e1927
Alberte Vieites <albertevieites@gmail.com>
2022-11-17 23:23:44 +0000

    feat: :sparkles: complete expenses route file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,7 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add").post((req, res) => {
+router.route("/").post((req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
