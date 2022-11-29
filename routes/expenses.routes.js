c10098bd2982968d2f4698b0630911b48602e945
Alberte Vieites <albertevieites@gmail.com>
2022-11-29 21:42:40 +0000

    fix: :bug: fix category and method type to array

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -2,50 +2,67 @@ const router = require("express").Router();
 const Expenses = require("../models/expense.model");
 
 // GET "/api/expenses" Send a GET request of the Expenses
-router.route("/").get((req, res) => {
-  Expenses.find()
-    .then((expenses) => res.json(expenses))
-    .catch((err) => res.status(400).json("Error: " + err));
+router.get("/", async (req, res, next) => {
+  try {
+    const allExpenses = await Expenses.find();
+    res.json(allExpenses);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // POST "/api/expenses" Send a POST request of the Expenses
-router.route("/").post((req, res) => {
+router.post("/", async (req, res, next) => {
   const { date, description, category, method, amount } = req.body;
 
-  const newExpense = new Expenses({
-    date,
-    description,
-    category,
-    method,
-    amount,
-  });
+  if (!date || !description || !category || !method || !amount === undefined) {
+    res.json({ errorMessage: "fields not provided" });
+  }
 
-  newExpense
-    .save()
-    .then(() => res.json("Expense added successfully!🌟"))
-    .catch((err) => res.status(400).json("Error: " + err));
+  try {
+    const newExpense = await Expenses.create({
+      date,
+      description,
+      category,
+      method,
+      amount,
+    });
+    res.json(newExpense);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET "/api/expenses/:id" Send details from a single expense
-router.route("/:id").get((req, res) => {
+router.get("/:id", async (req, res, next) => {
+  console.log(req.params);
+
   const { id } = req.params;
 
-  Expenses.findById(id)
-    .then((expenses) => res.json(expenses))
-    .catch((err) => res.status(400).json("Error: " + err));
+  try {
+    const singleExpense = await Expenses.findById(id);
+
+    res.json(singleExpense);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // DELETE "/api/expenses/:id" Delete a single expense in the Expenses
-router.route("/:id").delete((req, res) => {
+router.delete("/:id", async (req, res, next) => {
   const { id } = req.params;
 
-  Expenses.findByIdAndDelete(id)
-    .then(() => res.json("Expense record deleted!"))
-    .catch((err) => res.status(400).json("Error " + err));
+  try {
+    await Expenses.findByIdAndDelete(id);
+    // res.status(200).json();
+    res.json("expense deleted");
+  } catch (error) {
+    next(error);
+  }
 });
 
-// POST "/api/expenses/:id" Get changes and edit expense by id
-router.route("/:id").patch((req, res) => {
+// PATCH "/api/expenses/:id" Get changes, edit and update expense by id
+router.patch("/:id", async (req, res, next) => {
   const { id } = req.params;
   const { date, description, category, method, amount } = req.body;
 
@@ -53,20 +70,18 @@ router.route("/:id").patch((req, res) => {
     res.json({ errorMessage: "Fields not completed!" });
   }
 
-  Expenses.findByIdAndUpdate(id)
-    .then((expenses) => {
-      expenses.date = date;
-      expenses.description = description;
-      expenses.category = category;
-      expenses.method = method;
-      expenses.amount = amount;
-
-      expenses
-        .save()
-        .then(() => res.json("Expense record updated!"))
-        .catch((err) => res.status(400).json("Error: " + err));
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
+  try {
+    await Expenses.findByIdAndUpdate(id, {
+      date,
+      description,
+      category,
+      method,
+      amount,
+    });
+    res.json("updated expense successfully");
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
